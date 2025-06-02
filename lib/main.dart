ab0b347b8ffc3944cc8f6cad31e2feeed1a9150c
Benediktus Arthur Suparto <arthursuparto@gmail.com>
2025-06-02 16:10:06 +0700

    add home pagae and schedule page

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'pages/home_page.dart'; // adjust path if needed
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BeeHealthy',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepOrange),
+        useMaterial3: true,
+      ),
+      home: const HomePage(), 
+    );
+  }
+}
